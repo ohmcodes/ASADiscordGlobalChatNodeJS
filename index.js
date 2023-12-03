078c5ceb1e3c1d0461acadbe4cf1559cd1c8bc7f
Ohmcodes <ohmcodes@gmail.com>
2023-12-03 01:14:10 -0800

    add logger and optimize rcon connection

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const { Rcon } = require('rcon-client');
 const { Client, Events, GatewayIntentBits} = require('discord.js');
 
+var rcon;
+
 // Create a new client instance
 const client = new Client({ intents: [
   GatewayIntentBits.Guilds, 
@@ -10,47 +12,8 @@ const client = new Client({ intents: [
 ]});
 
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Ready! Logged in as ${c.user.tag}`)
 });
-
-// RCON configuration
-const rconConfig = {
-  host: process.env.HOST,
-  port: process.env.PORT,
-  password: process.env.RCONPWD,
-};
-
-// Function to send an RCON command
-async function sendRconCommand(command) {
-  try {
-    // Connect to the RCON server
-    const rcon = new Rcon(rconConfig);
-    await rcon.connect();
-
-    // Send the command
-    const response = await rcon.send(command);
-    parseMessage(response);
-    // Disconnect from the server
-    await rcon.end();
-  } catch (error) {
-    //console.error('Error:', error);
-    setTimeout(async ()=> {
-      await sendRconCommand(command);
-    }, 5000)
-  }
-}
-
-async function parseMessage(res){
-  var msgFilter = `${process.env.FILTERS}`.split(',');
-  if(!res.includes("Server received, But no response!!")){
-    const channel = client.channels.cache.find(channel => channel.id === process.env.CHANNEL_ID)
-    
-    if (!msgFilter.some(v => res.includes(v))) {
-        // removed double names eg: name (name): <msg>
-        channel.send(`${process.env.MAP}${res.replace(/ *\([^)]*\) */g, "")}`);
-    }
-  }
-}
 
 client.on("messageCreate", (message) => {
     if(message.author.bot) return;
@@ -68,12 +31,60 @@ client.on("messageCreate", (message) => {
     msg += ": "
     msg += `${message.content}`
     msg += `${(process.env.SUFFIX !="")? `${process.env.SUFFIX}`: ""}`
-
+    logger(msg)
     sendRconCommand(msg);
 })
 
+async function rConnect(){
+  try {
+    // Connect to the RCON server
+    rcon = new Rcon({
+      host: process.env.HOST,
+      port: process.env.PORT,
+      password: process.env.RCONPWD,
+    });
+    
+    await rcon.connect().then(()=>{
+      logger(`RCON is connected ${process.env.HOST}`)
+    })
+  } catch (error) {
+    logger(`Error RCON connection on ${process.env.HOST} Error: ${error}`)
+  }
+}
+
+// Function to send an RCON command
+async function sendRconCommand(command) {
+  try {
+    // Send the command
+    await rcon.send(command).then((res)=>{
+      parseMessage(res)
+    })
+  } catch (error) {
+    logger(`Error sending command: ${error}`)
+  }
+}
+
+async function parseMessage(res){
+  var msgFilter = `${process.env.FILTERS}`.split(',');
+  
+  if(!res.includes("Server received, But no response!!")){
+    const channel = client.channels.cache.find(channel => channel.id === process.env.CHANNEL_ID)
+    
+    if (!msgFilter.some(v => res.includes(v))) {
+        logger(res.trim())
+        channel.send(`${process.env.MAP}${res.trim()}`)
+    }
+  }
+}
+
+function logger(msg){
+  if(process.env.DEBUG=="true")
+    console.log(msg)
+}
+
+rConnect();
 setInterval(function() {
   sendRconCommand("GetChat");
-}, 1000)
+}, parseInt(process.env.INTERVAL))
 
 client.login(process.env.TOKEN);
